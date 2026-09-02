d5c1c5cabd1623d6c49d000a612438f990a24cac
bhenjayamparo-ctrl <bhenjayamparo@gmail.com>
2026-09-03 01:25:17 +0800

    Fix: correct view name in UsersController (users_view instead of users)

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -8,6 +8,6 @@ class UsersController extends Controller
         $this->call->model('UsersModel');
         $data['users'] = $this->UsersModel->all();
 
-        $this->call->view('users', $data);
+        $this->call->view('users_view', $data);
     }
 }
