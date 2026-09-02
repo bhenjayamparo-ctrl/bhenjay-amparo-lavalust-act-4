3b901ee1860dc633c3e0376ed316297a1a91e14c
bhenjayamparo-ctrl <bhenjayamparo@gmail.com>
2026-09-03 01:12:35 +0800

    Update Users files

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -3,16 +3,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 class UsersController extends Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-        $this->call->database();
-        $this->call->model('UsersModel');
-    }
-
     public function index()
     {
+        $this->call->model('UsersModel');
         $data['users'] = $this->UsersModel->all();
-        $this->call->view('users_view', $data);
+
+        $this->call->view('users', $data);
     }
 }
--- app/views/users_view.php
+++ app/views/users_view.php
@@ -1,31 +1,28 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Users List</title>
+    <meta charset="UTF-8">
+    <title>Users</title>
 </head>
 <body>
-    <h1>User Management Module</h1>
-    <table border="1" cellpadding="8" cellspacing="0">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Username</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($users as $user): ?>
-            <tr>
-                <td><?= $user->id; ?></td>
-                <td><?= $user->firstname; ?></td>
-                <td><?= $user->lastname; ?></td>
-                <td><?= $user->email; ?></td>
-                <td><?= $user->username; ?></td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
+    <h1>Users</h1>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Username</th>
+        </tr>
+        <?php foreach ($users as $user): ?>
+        <tr>
+            <td><?= $user['id'] ?></td>
+            <td><?= $user['firstname'] ?></td>
+            <td><?= $user['lastname'] ?></td>
+            <td><?= $user['email'] ?></td>
+            <td><?= $user['username'] ?></td>
+        </tr>
+        <?php endforeach; ?>
     </table>
 </body>
 </html>
