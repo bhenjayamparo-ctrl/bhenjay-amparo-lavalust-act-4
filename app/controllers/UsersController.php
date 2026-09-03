d980c2a7d1fb45a22931058a6fd0e9b57f5fc030
bhenjayamparo-ctrl <bhenjayamparo@gmail.com>
2026-09-03 08:03:44 +0800

    Fix: load database library in UsersController constructor

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -3,11 +3,16 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->call->model('UsersModel');
+        $this->call->database();
+    }
+
     public function index()
     {
-        $this->call->model('UsersModel');
         $data['users'] = $this->UsersModel->all();
-
         $this->call->view('users', $data);
     }
 }
